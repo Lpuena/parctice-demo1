e4d2114be296b20bb9c8b5b1b972b6916fe19401
haoyu-ge <g2534160060@163.com>
2022-08-16 11:32:53 +0800

    基本完成项目

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@
         <p>查询条件</p>
       </div>
       <div class="header-box-bottom">
-        <el-form :inline="true" :model="formInline" class="demo-form-inline from-css">
+        <el-form :inline="true" :model="formInline2" class="demo-form-inline from-css">
             <el-form-item label="名称"  class="form-item-css">
-              <el-input v-model="formInline.name" placeholder="请输入名称"></el-input>
+              <el-input v-model="formInline2.name" placeholder="请输入名称"></el-input>
             </el-form-item>
             <el-form-item label="类型" class="form-item-css">
-              <el-select v-model="formInline.type" placeholder="类型">
-                <el-option label="软件" value="Software"></el-option>
-                <el-option label="硬件" value="Hardware"></el-option>
-                <el-option label="集成" value="integrate"></el-option>
+              <el-select v-model="formInline2.type" placeholder="请选择类型" @change="aaa">
+                <el-option label="软件" value="1"></el-option>
+                <el-option label="硬件" value="2"></el-option>
+                <el-option label="集成" value="3"></el-option>
               </el-select>
             </el-form-item>
             <el-form-item>
@@ -26,10 +26,69 @@
     <div class="mid">
       <el-row class="marginBottom">
         <el-col :span="21">
-          <el-button plain >添加</el-button>
+          <el-button plain  @click="dialogVisible = true">新增</el-button>
         </el-col>
+          <el-dialog
+            title="新增合同"
+            :visible.sync="dialogVisible"
+            width="40%"
+            :before-close="handleClose">
+            <!-- <span>这是一段信息</span> -->
+            <!-- <el-row> -->
+            <el-form :inline="true" :model="formInline" class="demo-form-inline from-css"  label-width="60px">
+              <el-row>
+                <el-col :span="12">
+                  <el-form-item label="名称"  class="form-item-css">
+                    <el-input v-model="formInline.name" placeholder="请输入名称"></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item label="类型" class="form-item-css">
+                    <el-select v-model="formInline.type" placeholder="请选择类型" @change="aaa">
+                      <el-option label="软件" value="1"></el-option>
+                      <el-option label="硬件" value="2"></el-option>
+                      <el-option label="集成" value="3"></el-option>
+                    </el-select>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+
+              <el-row>
+                <el-col :span="12">
+                  <el-form-item  label="成本"  class="form-item-css">
+                    <el-input v-model="formInline.cost" placeholder="请输入成本" @blur="createPrice"></el-input>
+                  </el-form-item>
+                </el-col>
+                <el-col :span="12">
+                  <el-form-item  label="利润率"  class="form-item-css">
+                    <el-input v-model="formInline.profitMargin" placeholder="请输入利润率" @blur="createPrice"></el-input>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+
+              <el-row>
+                <el-col >
+                  <el-form-item  label="报价"  class="form-item-css" >
+                  <el-input v-model="formInline.price" placeholder="无需输入，报价自动生成" disabled></el-input>
+                </el-form-item>
+                </el-col>
+                <!-- <el-col :span="4">
+                  <el-form-item>
+                  <el-button type="primary" @click="onSubmit">查询</el-button>
+                </el-form-item>
+                </el-col> -->
+              </el-row>
+              </el-form>
+
+            <!-- </el-row> -->
+            <span slot="footer" class="dialog-footer">
+              <!-- <el-button @click="cancel">取 消</el-button> -->
+              <el-button type="primary"  @click="save">确 定</el-button>
+            </span>
+          </el-dialog>
+
         <el-col :span="3">
-          <el-input v-model="input" placeholder="请输入ID,回车进行搜索" @keydown.enter.native="searchById(input)"></el-input>
+          <!-- <el-input v-model="input" placeholder="请输入ID,回车进行搜索" @keydown.enter.native="searchById(input)"></el-input> -->
         </el-col>
       </el-row>
       <div class="mid-content">
@@ -61,10 +120,10 @@
                 width="120">
                 <template slot-scope="scope">
                   <el-button
-                    @click.native.prevent="deleteRow(scope.$index, tableData)"
+                    @click.native.prevent="handleRow(scope.row.id)"
                     type="text"
                     size="small">
-                    移除
+                    编辑
                   </el-button>
                 </template>
               </el-table-column>
@@ -91,17 +150,30 @@
 <script>
 export default {
   created() {
-    this.getTableData()
+    this.getTableData(this.formInline)
   },
 
   data() {
     return {
+      dialogVisible: false,
       input: '',
       currentPage: 1,
       pageSize: 10,
       formInline: {
+        id: '',
         name: '',
-        type: ''
+        type: '',
+        cost: '',
+        profitMargin: '',
+        price: ''
+      },
+      formInline2: {
+        id: '',
+        name: '',
+        type: '',
+        cost: '',
+        profitMargin: '',
+        price: ''
       },
       tableData: [],
       id: 1
@@ -119,12 +191,27 @@ export default {
         d = d < 10 ? ('0' + d) : d
         return y + '-' + m + '-' + d
       }
+    },
+    quotePrice() {
+      return Number(this.formInline.cost) + Number(this.formInline.cost) * Number(this.formInline.profitMargin)
     }
   },
   methods: {
+    // dialog 界面关闭功能
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+          Object.keys(this.formInline).forEach(x => { this.formInline[x] = '' })
+        })
+        .catch(_ => {})
+    },
+    aaa() {
+      console.log(this.formInline)
+    },
     // 批量获取合同信息
-    async getTableData() {
-      const { data: res } = await this.$http.post('/purchase/offer/query.do', {})
+    async getTableData(formInline) {
+      const { data: res } = await this.$http.post('/purchase/offer/query.do', (formInline))
       console.log(res)
       this.tableData = res.data
       this.changeType()
@@ -144,6 +231,7 @@ export default {
         }
       })
     },
+    // 时间进行转换
     changeTime() {
       this.tableData.forEach(x => {
         x.signTime = this.timeConersion(x.signTime)
@@ -153,26 +241,66 @@ export default {
     async searchById(id) {
       const { data: res } = await this.$http.get('/purchase/offer/getById.do?id=' + id)
       console.log(res)
-      this.input = ''
-      this.tableData = res.data
-      this.changeType()
-      this.changeTime()
+      console.log(res.data[0].id)
+      this.formInline = res.data[0]
+      // this.input = ''
+      // this.tableData = res.data
+      // this.changeType()
+      // this.changeTime()
     },
     // async getTableData() {
     //   const { res } = await this.$http.get('/purchase/offer/getById.do?id=' + this.id)
     //   console.log(res)
     // },
+    // 按条件进行查询
     onSubmit() {
       console.log('submit!')
+      // console.log(this.formInline.name)
+      // console.log(this.formInline.type)
+      this.getTableData(this.formInline2)
+      this.formInline2.name = ''
+      this.formInline2.type = ''
     },
-    deleteRow(index, rows) {
-      rows.splice(index, 1)
+    // 生成报价的价格
+    createPrice() {
+      if (this.formInline.profitMargin) {
+        this.formInline.price = this.quotePrice
+      }
+
+      // console.log(this.quotePrice)
+    },
+    handleRow(id) {
+      // rows.splice(index, 1)
+      this.dialogVisible = true
+      console.log(id)
+      this.searchById(id)
+      // console.log(rows)
     },
     handleSizeChange(val) {
       console.log(`每页 ${val} 条`)
     },
     handleCurrentChange(val) {
       console.log(`当前页: ${val}`)
+    },
+    cancel() {
+      this.dialogVisible = false
+      Object.keys(this.formInline).forEach(x => { this.formInline[x] = '' })
+    },
+    // 保存合同信息
+    save() {
+      this.dialogVisible = false
+      if (this.formInline.id === '') {
+        this.formInline.id = -1
+      }
+      console.log(this.formInline)
+      this.saveMessage(this.formInline)
+      Object.keys(this.formInline).forEach(x => { this.formInline[x] = '' })
+      this.getTableData(this.formInline)
+    },
+    // 调用接口发送要保存的信息
+    async saveMessage(formInline) {
+      const { data: res } = await this.$http.post('/purchase/offer/save.do', (formInline))
+      console.log(res)
     }
   }
 }
@@ -185,8 +313,6 @@ html,body {
 }
 .marginBottom {
   margin-bottom: 10px;
-  // display: flex;
-  // justify-content: space-between;
 }
 .app-container {
   margin-top: 10px;
